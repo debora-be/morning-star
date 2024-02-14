0afb1a02e2daf5a62eb9ecdccedadc513513116e
debora berte <arobedbee@gmail.com>
2024-02-14 16:49:51 -0300

    Chore: resolver retrieves only one image in time

--- lib/morning_star_web/graphql/resolvers/venus_resolver.ex
+++ lib/morning_star_web/graphql/resolvers/venus_resolver.ex
@@ -4,12 +4,10 @@ defmodule MorningStarWeb.Graphql.Resolvers.VenusResolver do
   """
 
   def get_venus_images(_parent, args, _info) do
-    start_date = Date.from_iso8601(args.start_date)
-    end_date = Date.from_iso8601(args.end_date)
+    start_date = Date.from_iso8601!(args.start_date)
+    end_date = Date.from_iso8601!(args.end_date)
 
-    with {:ok, start_date} <- start_date,
-         {:ok, end_date} <- end_date,
-         {:ok, response} <- MorningStar.Adapters.SpaceshipAgencyClient.get_venus_data(),
+    with {:ok, response} <- MorningStar.Adapters.SpaceshipAgencyClient.get_venus_data(),
          {:ok, decoded_response} <- Jason.decode(response.body) do
       parsed_data =
         MorningStar.Parsers.ParseVenusData.parse_data_for_period(
@@ -17,10 +15,15 @@ defmodule MorningStarWeb.Graphql.Resolvers.VenusResolver do
           end_date,
           decoded_response["collection"]["items"]
         )
+        |> Enum.take_random(1)
 
       {:ok, parsed_data}
     else
-      _error -> {:error, "Failed to fetch Venus images"}
+      _error -> {:error, :bad_request}
     end
+  end
+
+  def get_venus_stories(_parent, _args, _info) do
+    MorningStar.Models.VenusQueries.choose_story()
   end
 end
